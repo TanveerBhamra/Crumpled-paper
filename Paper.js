1b4f9c0b6146ba1ea2573f9957ee1f6d45efb126
TanveerBhamra <81157724+TanveerBhamra@users.noreply.github.com>
2021-06-07 15:39:36 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.3,
+          'friction':0,
+          'density':1.2
+      }
+
+      this.radius = 25;
+      this.body = Bodies.circle(x, y, this.radius, options);
+      this.image = loadImage("paper.png");
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      ellipse(0, 0, this.radius, this.radius);
+      pop();
+      imageMode(CENTER);
+			image(this.image,pos.x,pos.y);
+    }
+  };
+  
